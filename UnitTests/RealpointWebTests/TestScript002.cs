830a7962d04e46eea26263a66b52c7ccb93d70f9
brobinsondk <brobinsondk@msn.com>
2020-06-20 12:45:02 +0100

    RealPointWebTests : TC002 added, small hardening in TC004

--- UnitTests/RealpointWebTests/TestScript002.cs
+++ UnitTests/RealpointWebTests/TestScript002.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="TestScript004.cs" company="Mir Software">
+// <copyright file="TestScript002.cs" company="Mir Software">
 //   Copyright governed by Artistic license as described here:
 //          http://www.perlfoundation.org/artistic_license_2_0
 // </copyright>
@@ -17,10 +17,10 @@ namespace Realpoint.Stf.WebTests
     using RealpointWebTests;
 
     /// <summary>
-    /// The test script 004.
+    /// The test script 002.
     /// </summary>
     [TestClass]
-    public class TestScript004 : RealpointTestScriptBase
+    public class TestScript002 : RealpointTestScriptBase
     {
         /// <summary>
         /// The test initialize.
@@ -32,36 +32,15 @@ namespace Realpoint.Stf.WebTests
         }
 
         /// <summary>
-        /// The test top menu.
+        /// The test script 002
         /// </summary>
         [TestMethod]
-        public void Ts004()
+        public void Ts002()
         {
-            RealpointShell.MenuManager.DemoMode = true;
-            RealpointShell.MenuManager.ScreenShot= true;
-
-            var home = RealpointShell.Home();
-
-            StfAssert.IsInstanceOfType("Home", home, typeof(IHome));
-
-            var fractionalOwnership = RealpointShell.FractionalOwnership();
-            var ourService = RealpointShell.OurService();
-            var propertySearch = RealpointShell.PropertySearch();
-            var discoverItaly = RealpointShell.DiscoverItaly();
-            var rentals = RealpointShell.Rentals();
-            var blog = RealpointShell.Blog();
             var contact = RealpointShell.Contact();
-        }
 
-        /// <summary>
-        /// The sleep.
-        /// </summary>
-        /// <param name="seconds">
-        /// The seconds.
-        /// </param>
-        public void Sleep(int seconds)
-        {
-            System.Threading.Thread.Sleep(seconds * 1000);
+            contact.Name = "Fred"; 
+            contact.Send();
         }
     }
 }
