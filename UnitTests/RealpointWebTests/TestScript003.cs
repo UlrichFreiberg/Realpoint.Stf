6524f10a295a9bb1e2542913d583fa2b8cb8c2ad
brobinsondk <brobinsondk@msn.com>
2020-06-28 19:26:00 +0200

    RealpointWebTests: Added test script TestScript003.cs

---
+++ UnitTests/RealpointWebTests/TestScript003.cs
@@ -0,0 +1,55 @@
+﻿// <copyright file="TestScript003.cs" company="Mir Software">
+//   Copyright governed by Artistic license as described here:
+//          http://www.perlfoundation.org/artistic_license_2_0
+// </copyright>
+// <summary>
+//   Defines the TestScript001 type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Realpoint.Stf.RealpointWeb.Interfaces;
+using RealpointWebTests;
+
+namespace Realpoint.Stf.WebTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Mir.Stf.Utilities;
+
+    /// <summary>
+    /// The TestScript003
+    /// </summary>
+    [TestClass]
+    public class TestScript003 : RealpointTestScriptBase
+    {
+        /// <summary>
+        /// The test initialize.
+        /// </summary>
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            RealpointShell = Get<IRealpointWebShell>();
+            StfAssert.IsNotNull("RealpointShell", RealpointShell);
+        }
+
+        /// <summary>
+        /// The test clean up.
+        /// </summary>
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            RealpointShell?.CloseDown();
+        }
+
+        [TestMethod]
+        public void Ts003()
+        {
+            var discoverItaly = RealpointShell.DiscoverItaly();
+            var region = discoverItaly.SelectRegion("Puglia");
+            var regionName = region.Name;
+
+            // Check that we are on Pugli page
+            StfAssert.StringEqualsCi("Region is indeed Puglia", "Puglia", regionName);
+        }
+    }
+}
