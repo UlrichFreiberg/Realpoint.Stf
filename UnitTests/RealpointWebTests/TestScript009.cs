e409521526d42f485328d482ceee76ae6b840d5c
brobinsondk <brobinsondk@msn.com>
2020-06-28 19:22:04 +0200

    TestScript009 : Cleanup to create methods to encapsulate implementation details from test script. Corrections to comments.

--- UnitTests/RealpointWebTests/TestScript009.cs
+++ UnitTests/RealpointWebTests/TestScript009.cs
@@ -18,7 +18,7 @@ namespace Realpoint.Stf.WebTests
     using RealpointWebTests;
 
     /// <summary>
-    /// The test script 002.
+    /// The test script 009.
     /// </summary>
     [TestClass]
     public class TestScript009 : RealpointTestScriptBase
@@ -33,18 +33,27 @@ namespace Realpoint.Stf.WebTests
         }
 
         /// <summary>
-        /// The test script 002
+        /// The test script 009
         /// </summary>
         [TestMethod]
         public void Ts009()
         {
-            var contact = RealpointShell.FractionalOwnership().FractionalOwnershipContact();
+            var contact = GetContact();
 
             StfAssert.AreEqual("Checking that Message field is preset", "Please contact me about Fractional Ownership property in Italy", contact.Message);
 
-            contact.Name = "Bill"; 
+            contact.Name = "Bill";
             contact.Send();
 
+            CheckExpectedValidationErrors(contact);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="contact"></param>
+        private void CheckExpectedValidationErrors(IFractionalOwnershipContact contact)
+        {
             var validationErrors = contact.ValidationErrorsPresent();
 
             StfAssert.IsTrue("Validation Errors are present", validationErrors);
@@ -54,6 +63,15 @@ namespace Realpoint.Stf.WebTests
             StfAssert.IsTrue("Validation Errors are present for Email", missingFields.HasFlag(MissingRequiredFields.Email));
             //StfAssert.IsTrue("Validation Errors are present for Message", missingFields.HasFlag(MissingRequiredFields.Message));
             StfAssert.IsTrue("Validation Errors are present for Terms of use and Privacy policy", missingFields.HasFlag(MissingRequiredFields.TermsAndPrivacy));
+        }
+
+        private IFractionalOwnershipContact GetContact()
+        {
+            var fractionalOwnership = RealpointShell.FractionalOwnership();
+            var contact = fractionalOwnership?.FractionalOwnershipContact();
+
+            StfAssert.IsInstanceOfType("Contact", contact, typeof(IFractionalOwnershipContact));
+            return contact;
         }
     }
 }
